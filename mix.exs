0812ed51751d56a5fa428bc44e8086fa0c995b66
João Gilberto Balsini Moura <joaobalsini@Joaos-MacBook-Pro-2.local>
2020-09-12 21:04:32 -0300

    Add phx.gen.auth

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule RealEstate.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phx_gen_auth, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 
