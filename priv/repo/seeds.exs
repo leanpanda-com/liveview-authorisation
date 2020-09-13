d0ab24b9e3d07918e6084c44915b91628eea25da
João Gilberto Balsini Moura <joaobalsini@Joaos-MacBook-Pro-2.local>
2020-09-13 06:54:08 -0300

    Add seeds to create user and admin

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+RealEstate.Accounts.register_admin(%{
+  email: "admin@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
+
+RealEstate.Accounts.register_user(%{
+  email: "user1@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
+
+RealEstate.Accounts.register_user(%{
+  email: "user2@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
